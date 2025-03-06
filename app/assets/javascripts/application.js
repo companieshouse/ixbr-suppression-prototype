46468f351a976a93c95ba70561e15edab908e3f6
mtomas-ch <119611289+mtomas-ch@users.noreply.github.com>
2025-03-06 09:29:08 +0000

    code added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,95 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
+
+function change_click(resultValue) {
+  if (resultValue == 'result_1') {
+    var result_one = document.getElementById('result_1');
+    var result_one_expanded = document.getElementById('result_1_expanded');
+
+    result_one.classList.add('govuk-visually-hidden');
+    result_one_expanded.classList.remove('govuk-visually-hidden');
+  
+  } else if (resultValue == 'result_2') {
+      var result_two = document.getElementById('result_2');
+      var result_two_expanded = document.getElementById('result_2_expanded');
+
+      result_two.classList.add('govuk-visually-hidden');
+      result_two_expanded.classList.remove('govuk-visually-hidden');
+
+  } else if (resultValue == 'result_3') {
+      var result_three = document.getElementById('result_3');
+      var result_three_expanded = document.getElementById('result_3_expanded');
+
+      result_three.classList.add('govuk-visually-hidden');
+      result_three_expanded.classList.remove('govuk-visually-hidden');
+
+  } else if (resultValue == 'result_4') {
+      var result_four = document.getElementById('result_4');
+      var result_four_expanded = document.getElementById('result_4_expanded');
+
+      result_four.classList.add('govuk-visually-hidden');
+      result_four_expanded.classList.remove('govuk-visually-hidden');
+  }
+}
+
+function save_click(resultValue) {
+  if (resultValue == 'result_1') {
+    var result_one = document.getElementById('result_1');
+    var result_one_expanded = document.getElementById('result_1_expanded');
+    var result_one_new_text = document.getElementById('result_1_textbox');
+    var result_one_text = document.getElementById('result_1_text');
+
+    result_one.classList.remove('govuk-visually-hidden');
+    result_one_expanded.classList.add('govuk-visually-hidden');
+
+    result_one_text.innerHTML = result_one_new_text.value;
+  
+  } else if (resultValue == 'result_2') {
+      var result_two = document.getElementById('result_2');
+      var result_two_expanded = document.getElementById('result_2_expanded');
+      var result_two_new_text = document.getElementById('result_2_textbox');
+      var result_two_text = document.getElementById('result_2_text');
+
+      result_two.classList.remove('govuk-visually-hidden');
+      result_two_expanded.classList.add('govuk-visually-hidden');
+
+      result_two_text.innerHTML = result_two_new_text.value;
+
+  } else if (resultValue == 'result_3') {
+      var result_three = document.getElementById('result_3');
+      var result_three_expanded = document.getElementById('result_3_expanded');
+      var result_three_new_text = document.getElementById('result_2_textbox');
+      var result_three_text = document.getElementById('result_2_text');
+
+      result_three.classList.remove('govuk-visually-hidden');
+      result_three_expanded.classList.add('govuk-visually-hidden');
+
+      result_three_text.innerHTML = result_three_new_text.value;
+
+  } else if (resultValue == 'result_4') {
+      var result_four = document.getElementById('result_4');
+      var result_four_expanded = document.getElementById('result_4_expanded');
+      var result_four_new_text = document.getElementById('result_2_textbox');
+      var result_four_text = document.getElementById('result_2_text');
+
+      result_four.classList.remove('govuk-visually-hidden');
+      result_four_expanded.classList.add('govuk-visually-hidden');
+
+      result_four_text.innerHTML = result_four_new_text.value;
+  }
+}
+
+
+function undo_click(changeValue) {
+  if (changeValue == 'change_1') {
+    var change_one = document.getElementById('change_1');
+
+    change_one.classList.add('govuk-visually-hidden');
+  
+  } else if (changeValue == 'change_2') {
+      var change_two = document.getElementById('change_2');
+
+      change_two.classList.add('govuk-visually-hidden');
+  }
+}
